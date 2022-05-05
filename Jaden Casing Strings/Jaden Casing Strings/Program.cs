a874f569425ff2b79f78cc1e666306a1412ac131
kodead1991 <kodead@mail.ru>
2022-05-05 16:33:20 +0300

    Initial commit

---
+++ Jaden Casing Strings/Jaden Casing Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Jaden_Casing_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToJadenCase("How can mirrors be real if our eyes aren't real"));
+        }
+
+        static string ToJadenCase(string phrase)
+        {
+            return String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1)));
+        }
+    }
+}
