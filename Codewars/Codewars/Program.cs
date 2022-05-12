82243ede554ccd18badc59b7449549f800846e4f
kodead1991 <kodead@mail.ru>
2022-05-12 16:22:32 +0300

    12\05\22

--- Codewars/Codewars/Program.cs
+++ Codewars/Codewars/Program.cs
@@ -324,11 +324,35 @@ namespace Codewars
 
             #region 1/n- Cycle
 
-            Console.WriteLine(Running(33) + " 2");
-            Console.WriteLine(Running(18118) + " -1");
-            Console.WriteLine(Running(69) + " 22");
-            Console.WriteLine(Running(197) + " 98");
-            Console.WriteLine(Running(65) + " -1");
+            //Let be n an integer prime with 10 e.g. 7.
+
+            //1 / 7 = 0.142857 142857 142857....We see that the
+            //decimal part has a cycle: 142857.The length of
+            //this cycle is 6.In the same way:
+
+            //1 / 11 = 0.09 09 09....Cycle length is 2.
+
+            //Task
+            //Given an integer n(n > 1) the function cycle(n)
+            //returns the length of the cycle if there is one otherwise
+            //(n and 10 not coprimes) return -1.
+
+            //Examples:
+            //cycle(5) = -1
+            //cycle(13) = 6-> 0.076923 076923 0769
+            //cycle(21) = 6-> 0.047619 047619 0476
+            //cycle(27) = 3-> 0.037 037 037 037 0370
+            //cycle(33) = 2-> 0.03 03 03 03 03 03 03 03
+            //cycle(37) = 3-> 0.027 027 027 027 027 0
+            //cycle(94) = -1
+            //Notes
+            ///cycle(22) = -1 since 1 / 22 ~0.0 45 45 45 45...
+
+            //Console.WriteLine(Running(33) + " 2");
+            //Console.WriteLine(Running(18118) + " -1");
+            //Console.WriteLine(Running(69) + " 22");
+            //Console.WriteLine(Running(197) + " 98");
+            //Console.WriteLine(Running(65) + " -1");
 
             #endregion
         }
@@ -336,11 +360,12 @@ namespace Codewars
         public static int Running(int n)
         {
             // your code
+            if (n % 2 == 0 || n % 5 == 0) return -1;
+
             var val = 1;
             var let = 0;
             while (true)
             {
-                if (n % 2 == 0 || n % 5 == 0) return -1;
                 val = val * 10 % n;
                 let++;
                 if (val == 1)
